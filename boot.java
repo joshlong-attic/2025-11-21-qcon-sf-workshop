698c46150aaa6a50f97e646d1bb1ab2b49cc1738
Josh Long <54473+joshlong@users.noreply.github.com>
2025-11-21 09:19:56 -0800

    boot

---
+++ boot.java
@@ -0,0 +1,29 @@
+//usr/bin/env jbang  "$0" "$@" ; exit $?
+//JAVA 25
+//DEPS org.springframework.boot:spring-boot-starter-web:3.5.5
+//DEPS https://github.com/scratches/spring-script
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+static class HelloController {
+
+    @GetMapping("/hi")
+    Map<String, String> hello() {
+        return Map.of("message", "hi");
+    }
+}
+
+@Bean
+ApplicationRunner standalone() {
+    return _ -> System.out.println("standalone!!!");
+}
+
+void main() {
+    SpringScript.run();
+}
